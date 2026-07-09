925eef6ab922b56cde50aee9fe34f00e8171abe1
Jean-Pierre S. <sojez1@gmail.com>
2026-07-09 12:37:17 -0400

    project initialzation

---
+++ src/main/java/com/jpstechno/stock_back/StockBackApplication.java
@@ -0,0 +1,13 @@
+package com.jpstechno.stock_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockBackApplication.class, args);
+	}
+
+}
